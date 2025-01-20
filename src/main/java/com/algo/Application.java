1b351b2447453cabbdc985737fbcd48b90e2a8cb
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2025-01-20 19:24:05 +0100

    added the themes related to the labyrinth creation

--- src/main/java/com/algo/Application.java
+++ src/main/java/com/algo/Application.java
@@ -7,7 +7,7 @@ import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(title = "api-labyrinth-server", version = "1.0", contact = @Contact(email = "support@add.tn")), components = @Components(securitySchemes = {
+@OpenAPIDefinition(info = @Info(title = "api-labyrinth", version = "1.0", contact = @Contact(email = "support@add.tn")), components = @Components(securitySchemes = {
 }))
 public class Application extends jakarta.ws.rs.core.Application {
 }
